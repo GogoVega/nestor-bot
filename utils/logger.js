19d6fa171371145657f116930b847867c5f33f77
GogoVega <ga.dandele@students.ephec.be>
2022-08-16 11:23:07 +0200

    Improved `logger` format

--- utils/logger.js
+++ utils/logger.js
@@ -1,11 +1,16 @@
 const { createLogger, format, transports } = require("winston");
-const { colorize, combine, simple, timestamp } = format;
+const { colorize, combine, printf, timestamp } = format;
+
+const myFormat = printf(({ level, message, timestamp }) => {
+	return `${timestamp} ${level}: ${message}`;
+});
 
 const logger = createLogger({
+	level: "debug",
 	exitOnError: false,
-	format: combine(timestamp({ format: "YYYY-MM-DD HH:mm:ss" }), simple()),
+	format: combine(timestamp({ format: "YYYY-MM-DD HH:mm:ss" }), myFormat),
 	transports: [
-		new transports.Console({ format: combine(colorize(), simple()) }),
+		new transports.Console({ format: combine(colorize(), myFormat) }),
 		new transports.File({ filename: "data/nestor-bot.log", level: "info" }),
 	],
 });
