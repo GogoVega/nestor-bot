10eb53dd13f5a4aa1c494a52cfdc602f2cfbc9ba
GogoVega <ga.dandele@students.ephec.be>
2022-09-06 11:40:34 +0200

    fix: `getCpuUsage` function

--- src/commands/process-infos.ts
+++ src/commands/process-infos.ts
@@ -1,37 +1,23 @@
 import { Colors, EmbedBuilder, SlashCommandBuilder } from "discord.js";
-import { Command } from "../types/collection";
 import process from "node:process";
+import { Command } from "../types/collection";
 
-function secNSec2ms(secNSec: number | [number, number]) {
-	if (Array.isArray(secNSec)) {
-		return secNSec[0] * 1000 + secNSec[1] / 1000000;
-	}
-	return secNSec / 1000;
-}
-// TODO: Not sure it works!
-function getCpuUsage() {
-	const startTime = process.hrtime();
-	const startUsage = process.cpuUsage();
-
-	const elapTime = process.hrtime(startTime);
-	const elapUsage = process.cpuUsage(startUsage);
-
-	const elapTimeMS = secNSec2ms(elapTime);
-	const elapUserMS = secNSec2ms(elapUsage.user);
-	const elapSystMS = secNSec2ms(elapUsage.system);
-
-	return Math.round((100 * (elapUserMS + elapSystMS)) / elapTimeMS);
-}
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const getCpuUsage = require("cpu-percentage");
 
 export const processCommand: Command = {
 	data: new SlashCommandBuilder().setName("process-infos").setDescription("Replies with process infos."),
 	async execute(interaction, client) {
+		const start = getCpuUsage();
 		const guildsCount = {
 			name: "Nombre totale de guilds",
 			value: `Le serveur contient \`${(await client.guilds.fetch()).size}\` guilds.`,
 		};
 		const ping = { name: "Ping du serveur", value: `Le ping est de \`${client.ws.ping}\` ms.` };
-		const cpuUsage = { name: "Utilisation du processeur (CPU)", value: `Le CPU est utilisé à \`${getCpuUsage()}\` %` };
+		const cpuUsage = {
+			name: "Utilisation du processeur (CPU)",
+			value: `Le CPU est utilisé à \`${getCpuUsage(start).percent}\` %`,
+		};
 		const memoryObject = Object.entries(process.memoryUsage()).map(
 			([key, value]) => `\n• **${key}** => \`${value / 1000000}\` MB`
 		);
